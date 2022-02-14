0d20db90648923c085fa3742d50ee2f34013d5a7
Brian DeVries <brian@brianjdevries.com>
2022-02-13 21:06:44 -0500

    convert testimonials to present tense

--- src/_data/testimonials.js
+++ src/_data/testimonials.js
@@ -9,7 +9,7 @@ module.exports = [
   {
     name: "Denise Blumberg",
     testimonial:
-      "I have truly enjoyed working with Brian. He is a true professional with strong communication skills. He is an experienced .NET developer who is often looked to for both difficult and creative work. Brian organizes his work so he can deliver on time and keeps me updated on his progress. He is down to earth and can translate tech jargon into business terms so his clients can understand. I am very lucky to have him on my team.",
+      "I truly enjoy working with Brian. He is a true professional with strong communication skills. He is an experienced .NET developer who is often looked to for both difficult and creative work. Brian organizes his work so he can deliver on time and keeps me updated on his progress. He is down to earth and can translate tech jargon into business terms so his clients can understand. I am very lucky to have him on my team.",
     dateReceived: new Date("2021-07-27"),
     title: "Delivery Manager",
     company: "Randstad",
@@ -18,7 +18,7 @@ module.exports = [
   {
     name: "Jeff Warnke",
     testimonial:
-      "Brian was a valued member of our team. He has demonstrated the motivation to learn, and was always willing to help others on the team. Brian recognizes that a good understanding of requirements is critical to successful delivery, and has been developing the skills to ensure understanding.",
+      "Brian is a valued member of our team. He demonstrates the motivation to learn, and is always willing to help others on the team. Brian recognizes that a good understanding of requirements is critical to successful delivery, and he continues to develop the skills to ensure understanding.",
     dateReceived: new Date("2021-08-13"),
     title: "Solution Development Manager",
     company: "Meijer",
@@ -27,7 +27,7 @@ module.exports = [
   {
     name: "Tanner Wysocki",
     testimonial:
-      "Brian has been a pleasure to work with professionally from day one. You will find that he carries himself with a friendly and approachable demeanor that makes him a breeze to work with. In addition, you will find that he is a knowledgeable developer, and he has the passion, curiosity, adaptability, and organizational skills to learn and fill in the gaps of knowledge he may have. I can say with confidence that Brian has the skills to succeed on your team.",
+      "Brian is a pleasure to work with professionally. He carries himself with a friendly and approachable demeanor that makes him a breeze to work with. In addition, he is a knowledgeable developer. He has the passion, curiosity, adaptability, and organizational skills to learn and fill in any gaps of knowledge. I can say with confidence that Brian has the skills to succeed.",
     dateReceived: new Date("2021-08-13"),
     title: "Software Engineer",
     company: "Meijer",
