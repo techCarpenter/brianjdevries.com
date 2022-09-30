4c60f66215c93937228eb327e3e713747df73d17
Brian DeVries <brian@brianjdevries.com>
2022-09-30 13:46:17 -0400

    move projects from markdown files to data file

--- src/_data/projectData.js
+++ src/_data/projectData.js
@@ -1,82 +1,50 @@
 module.exports = [
 	{
-		name: "Tribute page",
-		url: "https://techcarpenter.github.io/tributepage/",
-		image: ""
+		startDate: new Date("2020-05-15"),
+		endDate: null,
+		title: "Topple",
+		url: "https://topple.netlify.com",
+		repo: "https://github.com/techcarpenter/topple",
+		summary: `I have student debt and wanted to create a way to plan and track my progress paying it off. Most other solutions only create a projection, but I'm working to make an app that keeps up with you.
+
+#### Tech
+
+- Implemented Frontend with [Vue][vue] (including Vuex and Vue-router)
+  - Code base written primarily with [Typescript][typescript]
+  - [Plotly.js][plotlyjs] package for charts
+  - [Tailwind CSS][tailwind] used for styles
+- Data is persisted in [Firestore database][firestore]
+- Implemented user authentication with [Firebase Auth][firebase-auth]
+- Deployed on [Netlify][netlify]
+
+<!-- Links -->
+
+[plotlyjs]: https://plotly.com/javascript "Plotly.js website"
+[tailwind]: https://tailwindcss.com "Tailwind CSS website"
+[vue]: https://vuejs.org "Vue JS website"
+[typescript]: https://typescriptlang.org "Typescript website"
+[firestore]: https://firebase.google.com/products/firestore "Firebase firestore website"
+[firebase-auth]: https://firebase.google.com/products/auth "Firebase auth website"
+[netlify]: https://netlify.com "Netlify website"`
 	},
 	{
-		name: "Survey Form",
-		url: "https://techcarpenter.github.io/surveyform/",
-		image: ""
-	},
-	{
-		name: "Landing page",
-		url: "https://techcarpenter.github.io/landingpage/",
-		image: ""
-	},
-	{
-		name: "Technical Document",
-		url: "https://techcarpenter.github.io/technicaldocument/",
-		image: ""
-	},
-	{
-		name: "Personal Portfolio",
-		url: "https://techcarpenter.github.io/personalportfolio/",
-		image: ""
-	},
-	{
-		name: "JS calculator",
-		url: "https://techcarpenter.github.io/jscalculator/",
-		image: ""
-	},
-	{
-		name: "Pomodoro Clock",
-		url: "https://techcarpenter.github.io/pomodoroclock/",
-		image: ""
-	},
-	{
-		name: "Markdown Previewer",
-		url: "https://techcarpenter.github.io/markdownpreviewer/",
-		image: ""
-	},
-	{
-		name: "Drum machine",
-		url: "https://techcarpenter.github.io/drummachine/",
-		image: ""
-	},
-	{
-		name: "Random quote machine",
-		url: "https://techcarpenter.github.io/randomquotemachine/",
-		image: ""
-	},
-	{
-		name: "Choropleth visualization",
-		url: "https://techcarpenter.github.io/fcc-d3-choropleth/",
-		image: ""
-	},
-	{
-		name: "Treemap visualization",
-		url: "https://techcarpenter.github.io/fcc-d3-treemap/",
-		image: ""
-	},
-	{
-		name: "Heatmap visualization",
-		url: "https://techcarpenter.github.io/fcc-d3-heatmap/",
-		image: ""
-	},
-	{
-		name: "Scatterplot visualization",
-		url: "https://techcarpenter.github.io/fcc-d3-scatterplot/",
-		image: ""
-	},
-	{
-		name: "Bar chart visualization",
-		url: "https://techcarpenter.github.io/fcc-d3-bar-chart/",
-		image: ""
-	},
-	{
-		name: "Topple",
-		url: "https://techcarpenter.github.io/",
-		image: ""
+		startDate: new Date("2020-08-23"),
+		endDate: null,
+		title: "brianjdevries.com",
+		url: "https://brianjdevries.com",
+		repo: "https://github.com/techcarpenter/brianjdevries.com",
+		summary: `This is my personal website. It's a constant work in progress, but I enjoy trying new things with it!
+
+I also write occasionally on [my blog](/blog/).
+
+#### Tech
+
+- Built using [Eleventy][eleventy], a simpler static site generator
+- Deployed on [Netlify][netlify]
+
+<!-- Links -->
+
+[eleventy]: https://www.11ty.dev "Eleventy website"
+[netlify]: https://netlify.com "Netlify website"`
 	}
-];
+].sort((a, b) => a.startDate.getTime() - b.startDate.getTime());
