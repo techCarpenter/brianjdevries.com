5bb541bf10adfc25d98492e0b1148fe1f70cb274
Brian DeVries <brian@brianjdevries.com>
2022-10-05 07:00:19 -0400

    Clean up comments in .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -17,9 +17,12 @@ module.exports = function (config) {
 				ready: function (err, bs) {
 					bs.addMiddleware("*", (req, res) => {
 						const content_404 = fs.readFileSync("dist/404.html");
-						// Add 404 http status code in request header.
+
+						/**
+						 * Add 404 http status code in request header.
+						 */
 						res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
-						// Provides the 404 content without redirect.
+
 						res.write(content_404);
 						res.end();
 					});
@@ -27,29 +30,19 @@ module.exports = function (config) {
 			}
 		});
 
-		/* HTML VALIDATION */
-		/**
-		 * Only run html validation in development mode
-		 */
 		config.on("eleventy.after", htmlValidator.storeResults);
 		config.addLinter("html-validator", htmlValidator.validate);
 	}
 
-	/* PLUGINS */
 	config.addPlugin(require("@11ty/eleventy-navigation"));
 	config.addPlugin(require("@11ty/eleventy-plugin-syntaxhighlight"));
 
-	/* SHORTCODES */
 	config.addShortcode("navlist", require("./lib/shortcodes/navlist.js"));
 
-	/* COLLECTIONS */
 	config.addCollection("blog", function (collection) {
 		return collection.getFilteredByGlob("./src/blog/**/*.md");
 	});
 
-	/* FILTERS */
-
-	// format dates
 	config.addFilter("dateformat", require("./lib/filters/dateformat"));
 	config.addFilter("markdown", content => md.render(content));
 	config.addFilter("slugify", require("./lib/filters/slugify"));
@@ -58,7 +51,7 @@ module.exports = function (config) {
 	config.addPassthroughCopy({ "src/static": "/" });
 	config.addPassthroughCopy({ "src/blog/**/*.jpg": "/images" });
 
-	config.on("eleventy.after", require("./socialPreviewImages"))
+	config.on("eleventy.after", require("./socialPreviewImages"));
 
 	return {
 		dir: {
