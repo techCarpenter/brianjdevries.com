09527ea958b79dbc0eef86e90dc5e312e387f0b7
Brian DeVries <brian@brianjdevries.com>
2022-11-19 16:24:17 -0500

    Update new post helper script

--- setup-scripts/new-post.js
+++ setup-scripts/new-post.js
@@ -2,28 +2,24 @@ const fs = require("fs");
 const slugify = require("slugify").default;
 const { argv } = require("process");
 const { exec } = require("child_process");
+const dayjs = require("dayjs");
 
+const dateFormat = dayjs().format("YYYY-MM-DD");
 const postTitle = argv[2];
-const newDir = `./src/blog/${slugify(postTitle, {
-	lower: true
-})}`;
-const today = new Date();
+const fileName = slugify(`${dateFormat}-${postTitle}`, {
+  lower: true
+});
+const newFile = `./src/blog/${fileName}.md`;
 
 const boilerplate = `---
 title: "${postTitle}"
 author: "Brian DeVries"
 description: ""
-date: ${today.getFullYear().toString()}-${(today.getMonth() + 1)
-	.toString()
-	.padStart(2, "0")}-${today.getDate().toString()}
 tags: []
+draft: true
 ---
 `;
 
-if (!fs.existsSync(newDir)) {
-	fs.mkdirSync(newDir, { recursive: true });
-}
+fs.writeFileSync(newFile, boilerplate);
 
-fs.writeFileSync(`${newDir}/index.md`, boilerplate);
-
-exec(`code ${newDir}/index.md`);
+exec(`code ${newFile}`);
