8234022fab1d6bbacec9b8db3ecccdb4c48dcc47
Brian DeVries <brian@brianjdevries.com>
2024-12-11 21:42:01 -0500

    add certification images

--- src/_data/certifications.js
+++ src/_data/certifications.js
@@ -1,27 +1,37 @@
 module.exports = [
-	{
-		name: "Responsive Web Design",
-		url: "https://www.freecodecamp.org/certification/techcarpenter/responsive-web-design",
-		image: ""
-	},
-	{
-		name: "Javascript Algorithms and Data Structures",
-		url: "https://www.freecodecamp.org/certification/techcarpenter/javascript-algorithms-and-data-structures",
-		image: ""
-	},
-	{
-		name: "Frontend Libraries",
-		url: "https://www.freecodecamp.org/certification/techcarpenter/front-end-libraries",
-		image: ""
-	},
-	{
-		name: "Data Visualization",
-		url: "https://www.freecodecamp.org/certification/techcarpenter/data-visualization",
-		image: ""
-	},
-	{
-		name: "Certified SAFe 5 Practitioner",
-		url: "https://www.credly.com/badges/1276643e-f02d-481e-b8e5-5e90706434cc?source=linked_in_profile",
-		image: ""
-	}
+  {
+    name: "Responsive Web Design",
+    url: "https://www.freecodecamp.org/certification/techcarpenter/responsive-web-design",
+    image: "https://assets.vries.land/fcc-responsive-web-design-cert.jpg"
+  },
+  {
+    name: "Javascript Algorithms and Data Structures",
+    url: "https://www.freecodecamp.org/certification/techcarpenter/javascript-algorithms-and-data-structures",
+    image: "https://assets.vries.land/fcc-javascript-algorithms-and-data-structures-cert.jpg"
+  },
+  {
+    name: "Frontend Libraries",
+    url: "https://www.freecodecamp.org/certification/techcarpenter/front-end-libraries",
+    image: "https://assets.vries.land/fcc-frontend-development-libraries-cert.jpg"
+  },
+  {
+    name: "Data Visualization",
+    url: "https://www.freecodecamp.org/certification/techcarpenter/data-visualization",
+    image: "https://assets.vries.land/fcc-data-visualization-cert.jpg"
+  },
+  {
+    name: "Backend Development and APIs",
+    url: "https://www.freecodecamp.org/certification/techcarpenter/back-end-development-and-apis",
+    image: "https://assets.vries.land/fcc-backend-development-and-apis-cert.jpg"
+  },
+  {
+    name: "Relational Database",
+    url: "https://www.freecodecamp.org/certification/techcarpenter/relational-database-v8",
+    image: "https://assets.vries.land/fcc-relational-database-cert.jpg"
+  },
+  {
+    name: "Certified SAFe 5 Practitioner",
+    url: "https://www.credly.com/badges/1276643e-f02d-481e-b8e5-5e90706434cc?source=linked_in_profile",
+    image: ""
+  }
 ];
